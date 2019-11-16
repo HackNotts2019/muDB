1fb79fe75a55dcca8fc3e5c86a8b27c3e866af55
IcE <ivica-matic@outlook.com>
2019-11-16 13:30:05 +0000

    Added DatabaseManager class

---
+++ DatabaseManager.py
@@ -0,0 +1,33 @@
+class DatabaseManager:
+    def __init__(self):
+        pass
+    # Database methods #
+    def create_database(self):
+        pass
+    def drop_database(self):
+        pass
+    def rename_database(self):
+        pass
+    def list_tables(self):
+        pass
+    # Table methods #
+    def create_table(self):
+        pass
+    def drop_table(self):
+        pass
+    def rename_table(self):
+        pass
+    # Data manipulation methods #
+    def select_all(self):
+        pass
+    def select_by_key(self):
+        pass
+    def insert(self):
+        pass
+    def remove_all(self):
+        pass
+    def remove_by_key(self):
+        pass
+    def update_by_key(self):
+        pass
+
